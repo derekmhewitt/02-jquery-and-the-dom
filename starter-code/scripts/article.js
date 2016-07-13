23da7807a97a29cbe90b53398896181a5640a72a
James <canning.jimmy@gmail.com>
2016-07-12 17:08:01 -0700

    Compleated remaining TODO items

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,20 +15,8 @@ function Article (opts) {
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-  /*<section id="articles">
-  <article class="template" data-category="cat">
-  <header>
-  <h1>Title</h1>
-  <div class="byline">
-  By <address><a href="">Author Name</a></address>
-  published <time pubdate datetime="2000-01-01">Publish Time</time>
-  </div>
-  </header>
-  <section class="article-body"></section>
-  </article>
-  </section>*/
 
-  /* TODO: Now use jQuery to fill in the rest of the current
+  /* TODO: DONE Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
@@ -37,18 +25,23 @@ Article.prototype.toHtml = function() {
     4. article body, and
     5. publication date. */
 
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('address').html('<a href="' + this.authorUrl + '"">' + this.author + '</a>');
+  $newArticle.find('.article-body').html(this.body);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-  /* TODO: This clone article is no longer a template,
+  /* TODO: DONE This clone article is no longer a template,
   as it now has real data attached to it! We need to account
   for that before this current article gets rendered to our
   DOM. */
 
+  $newArticle.removeClass('template');
+
   return $newArticle;
 };
 
-ourLocalData.sort(function(curElem, nextElem) {
+ourLocalData.sort(function(a, b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
